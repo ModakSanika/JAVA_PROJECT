6fb1ea33b496097963bd4245be35972351cd5838
MODAK SANIKA <131785948+ModakSanika@users.noreply.github.com>
2024-08-23 21:41:02 +0530

    Delete Desktop directory

--- Desktop/Java_Assignment/assignment11/Generics.java
+++
@@ -1,6 +0,0 @@
-package assignment11;
-
-public interface Generics<T> {
-    T getValue();
-    void setValue(T value);
-}
--- Desktop/Java_Assignment/assignment11/Main.java
+++
@@ -1,9 +0,0 @@
-package assignment11;
-
-public class Main {
-    public static void main(String[] args) {
-        MyString myString = new MyString();
-        myString.setValue("Hello, Sanika!");
-        System.out.println(myString.getValue());
-    }
-}
