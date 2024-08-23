51c77169466ac36617c1d4cb092ea9f8fdd15aaf
MODAK SANIKA <131785948+ModakSanika@users.noreply.github.com>
2024-08-23 21:39:49 +0530

    Delete project-3 directory

--- project-3/index.js
+++
@@ -1,8 +0,0 @@
-const clock=document.querySelector('.showTime');
-setInterval(function (){
-let date = new Date();
-clock.innerHTML=date.toLocaleTimeString();
-clock.style.color='white';
-clock.style.textAlign = 'center'; 
-
-},1000);
